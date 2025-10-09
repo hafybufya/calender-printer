b6196cfe6320e64856d54def7b5b39659e7a33d2
yourusernamenamehafybufya <hafsaabdifitah06@gmail.com>
2025-10-09 21:36:23 +0100

    struggling to get the while statements to work. figuring out where to place return statemnts for day and month functions

--- calenderPrinter.py
+++ calenderPrinter.py
@@ -5,35 +5,52 @@
 
 day_in_week = 7 
 
+
+min_number_month_days = 28
+max_number_month_days = 31
+
+min_number_week_days = 1
+max_number_week_days: 7
+
 prompts_days_in_month= "How many days are in the month? "
 
 prompt_first_day = "What day of the week does the month start on? (Sun=1, Monday=2, ... Sat= 7) "
 
-min_number_month_days = 28
-max_number_month_days = 31
     #take user input: number of days in month
 
 #days_in_month = int(input( prompts_days_in_month))
 #days_in_month = 30
 
     #take user input: day the month starts on
-first_day = int(input(prompt_first_day))
+#first_day = int(input(prompt_first_day))
 #user cant input value > 7
 
 
-try:
-    days_in_month = int(input( prompts_days_in_month))
-except ValueError:
-    print(" Months cannot have partial days! Please enter a whole number between 28 - 31") #unhardcode numbers
-    days_in_month = int(input( prompts_days_in_month))
-except NameError:
-    #this part not working i thoguht it would work for string
-    print("Must be an integer! Please enter a whole number between 28 - 31 ") #is there a way for this to be repeated everytime instead?
-    first_day = int(input(prompt_first_day))
-else:
-        #takes number of days in the month
-    for i in range(1, days_in_month+1): 
-        print(f"{i:2}", end=" ") 
+while True:
+    try:
+        days_in_month = int(input( prompts_days_in_month))
+        if 28 <= days_in_month <= 31:
+            break
+        else:
+            print("Enter a value between 28 to 31")
+    except ValueError:
+        print("Please enter a whole number between 28 - 31") #unhardcode numbers
+        
+while True:
+    try:
+        first_day  = int(input(prompt_first_day))
+        if 1 <= first_day  <= 7 :
+            break
+        else:
+            print("Enter a value between 1 to 7")
+    except ValueError:
+        print("Please enter a whole number between 1 to 7") #unhardcode numbers
+        
+
+
+#     #takes number of days in the month
+# for i in range(1, days_in_month+1): 
+#     print(f"{i:2}", end=" ") 
 
 
 
@@ -50,13 +67,18 @@ else:
 for i in range(first_day-1):
     print("   ", end="") # end = "" ensure it prints on same line
 
+
+    #takes number of days in the month
+for i in range(1, days_in_month+1): 
+    print(f"{i:2}", end=" ") 
+
 #     #takes number of days in the month
 # for i in range(1, days_in_month+1): 
 #     print(f"{i:2}", end=" ") 
 
             #new line after every 7th day (including spaces)
-    if (i + first_day - 1) % day_in_week == 0:
-        print()  # newline
+if (i + first_day - 1) % day_in_week == 0:
+    print()  # newline
 
 
 #calls function

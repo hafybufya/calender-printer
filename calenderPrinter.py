71b61c5e7933fcdc617e93d3e7d0c9ca0db8ed2f
yourusernamenamehafybufya <hafsaabdifitah06@gmail.com>
2025-10-09 11:36:54 +0100

    including some error handling for days_in_month variable

--- calenderPrinter.py
+++ calenderPrinter.py
@@ -9,31 +9,54 @@ prompts_days_in_month= "How many days are in the month? "
 
 prompt_first_day = "What day of the week does the month start on? (Sun=1, Monday=2, ... Sat= 7) "
 
+min_number_month_days = 28
+max_number_month_days = 31
     #take user input: number of days in month
-days_in_month = int(input( prompts_days_in_month))
+
+#days_in_month = int(input( prompts_days_in_month))
+#days_in_month = 30
 
     #take user input: day the month starts on
 first_day = int(input(prompt_first_day))
 #user cant input value > 7
 
- # function to print calendar
-def calender_printer(days_in_month, first_day):
 
-    calender_title = ["S", "M" , "T", "W", "T", "F", "S"]
-    print("  ".join(calender_title)) 
-
-         #makes spaces based on first day of month
-    for i in range(first_day-1):
-        print("   ", end="") # end = "" ensure it prints on same line
-
+try:
+    days_in_month = int(input( prompts_days_in_month))
+except ValueError:
+    print(" Months cannot have partial days! Please enter a whole number between 28 - 31") #unhardcode numbers
+    days_in_month = int(input( prompts_days_in_month))
+except NameError:
+    #this part not working i thoguht it would work for string
+    print("Must be an integer! Please enter a whole number between 28 - 31 ") #is there a way for this to be repeated everytime instead?
+    first_day = int(input(prompt_first_day))
+else:
         #takes number of days in the month
     for i in range(1, days_in_month+1): 
         print(f"{i:2}", end=" ") 
 
-             #new line after every 7th day (including spaces)
-        if (i + first_day - 1) % day_in_week == 0:
-            print()  # newline
 
-    return calender_title
+
+
+ # function to print calendar
+
+
+#make sure this only prints if eveyrthing functional !!
+
+# calender_title = ["S", "M" , "T", "W", "T", "F", "S"]
+# print("  ".join(calender_title)) 
+
+        #makes spaces based on first day of month
+for i in range(first_day-1):
+    print("   ", end="") # end = "" ensure it prints on same line
+
+#     #takes number of days in the month
+# for i in range(1, days_in_month+1): 
+#     print(f"{i:2}", end=" ") 
+
+            #new line after every 7th day (including spaces)
+    if (i + first_day - 1) % day_in_week == 0:
+        print()  # newline
+
+
 #calls function
-calender_printer(days_in_month, first_day)
--- unitTest.py
+++ unitTest.py
@@ -11,7 +11,9 @@ class my_unit_tests(unittest.TestCase):
 
 #make sure days in month only equal 28, 29, 30, 31
 
-# make sure user cant input value greater than 7
+# make sure user cant input value greater than 7 for days
+
+#i wanna add a try thingy so maybe a self.assert(equal) to an error message of "Insert a number no larger than (days)"
 
     # run the tests
 if __name__ == "__main__":

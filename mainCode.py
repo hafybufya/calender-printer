d6bd5749260088c8d96dc87aeaa8de000522fa76
yourusernamenamehafybufya <hafsaabdifitah06@gmail.com>
2025-11-24 11:17:53 +0000

    modification: improved commenting on calender printer code

--- mainCode.py
+++ mainCode.py
@@ -1,19 +1,23 @@
-#variables
+# ---------------------------------------------------------------------
+# Defined variables and prompts at the beginning of the program
+#  -> make the code flexible if prompts or values changed changed
+# ---------------------------------------------------------------------
+
+# Values
 day_in_week = 7 
-
-
 min_number_month_days = 28
 max_number_month_days = 31
-
 min_number_week_days = 1
 max_number_week_days = 7
 
-#prompts
+# Prompts
 prompts_days_in_month= "How many days are in the month? "
 prompt_first_day = "What day of the week does the month start on? (Sun=1, Mon=2, ..., Sat= 7) "
 prompt_error_handling = "Enter a value between "
 
-
+# ---------------------------------------------------------------------
+# FUNCTION: Get number of days in month
+# ---------------------------------------------------------------------
 
 def get_days_in_month():
     """function which gets the number of days in a month from users and preforms error handling """   
@@ -30,6 +34,9 @@ def get_days_in_month():
         except ValueError:
             print(f"{prompt_error_handling} {min_number_month_days} to {max_number_month_days}")
         
+# ---------------------------------------------------------------------
+# FUNCTION: Get number of days in week
+# ---------------------------------------------------------------------
 
 def get_days_in_week():
     """function which gets the day of the week the month starts on and preforms error handling """               
@@ -42,30 +49,34 @@ def get_days_in_week():
                 print(f"{prompt_error_handling} {min_number_week_days} to {max_number_week_days}")
         except ValueError:
             print(f"{prompt_error_handling} {min_number_week_days} to {max_number_week_days}")
+    
 
-        
-
+# ---------------------------------------------------------------------
+# FUNCTION: Calender printer
+# ---------------------------------------------------------------------
 
 def calender_printer(days_in_month, first_day):
     """function which prints a calender""" 
     calender_title = [" S", "M" , "T", "W", "T", "F", "S"]
     print("  ".join(calender_title)) 
 
-         #makes spaces/gaps based on first day of month
+   # Makes spaces/gaps based on first day of month
     for i in range(first_day-1):
-        print("   ", end="") # end = "" ensure it prints on same line
+        print("   ", end="") # Ensures it prints on same line
 
-        #takes number of days in the month
+    # Takes number of days in the month
     for i in range(1, days_in_month+1): 
         print(f"{i:2}", end=" ") 
 
-             #new line after every 7th day (including spaces)
+        # New line after every 7th day (including spaces)
         if (i + first_day - 1) % day_in_week == 0:
             print()  # newline
 
     return calender_title
 
-#calls function
-days_in_month = get_days_in_month()
-first_day = get_days_in_week()
-calender_printer(days_in_month, first_day)
+if __name__ == "__main__":
+
+    days_in_month = get_days_in_month()
+    first_day = get_days_in_week()
+    calender_printer(days_in_month, first_day)
+        

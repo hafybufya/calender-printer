d445337044b376b1dc7448e20613fdce383597de
yourusernamenamehafybufya <hafsaabdifitah06@gmail.com>
2025-11-24 15:34:10 +0000

    modification: functions error testing can now be unit tested

--- mainCode.py
+++ mainCode.py
@@ -4,7 +4,7 @@
 # ---------------------------------------------------------------------
 
 # Values
-day_in_week = 7 
+day_in_week = 7
 min_number_month_days = 28
 max_number_month_days = 31
 min_number_week_days = 1
@@ -13,7 +13,9 @@ max_number_week_days = 7
 # Prompts
 prompts_days_in_month= "How many days are in the month? "
 prompt_first_day = "What day of the week does the month start on? (Sun=1, Mon=2, ..., Sat= 7) "
-prompt_error_handling = "Enter a value between "
+
+prompt_error_handling_month = f"Enter a value between {min_number_month_days} to {max_number_month_days}"
+prompt_error_handling_week = f"Enter a value between {min_number_week_days} to {max_number_week_days}"
 
 # ---------------------------------------------------------------------
 # FUNCTION: Get number of days in month
@@ -23,17 +25,16 @@ def get_days_in_month():
     """function which gets the number of days in a month from users and preforms error handling """   
     while True:
         try:
-            days_in_month = int(input( prompts_days_in_month))
-            #user can only input a number within a specific range
-            if min_number_month_days <= days_in_month <= max_number_month_days:
-                #passed into calender_printer()
-                return days_in_month
-            else:
-                print(f"{prompt_error_handling} {min_number_month_days} to {max_number_month_days}")
-        #users can't input non-integers        
-        except ValueError:
-            print(f"{prompt_error_handling} {min_number_month_days} to {max_number_month_days}")
+            days_in_month = int(input(prompts_days_in_month))
+            
+        except ValueError: #users can't input non-integers
+            raise ValueError(f"{prompt_error_handling_month}")
+
+        if days_in_month < min_number_month_days or days_in_month > max_number_month_days:
+            raise ValueError(f"{prompt_error_handling_month}")
         
+        return days_in_month
+
 # ---------------------------------------------------------------------
 # FUNCTION: Get number of days in week
 # ---------------------------------------------------------------------
@@ -43,13 +44,12 @@ def get_days_in_week():
     while True:
         try:
             first_day  = int(input(prompt_first_day))
-            if min_number_week_days <= first_day  <= max_number_week_days :
-                return first_day
-            else:
-                print(f"{prompt_error_handling} {min_number_week_days} to {max_number_week_days}")
         except ValueError:
-            print(f"{prompt_error_handling} {min_number_week_days} to {max_number_week_days}")
-    
+            raise ValueError(f"{prompt_error_handling_week}")
+              
+        if first_day < min_number_week_days or first_day > max_number_week_days:
+            raise ValueError(f"{prompt_error_handling_week}")   
+        return first_day
 
 # ---------------------------------------------------------------------
 # FUNCTION: Calender printer
@@ -71,7 +71,6 @@ def calender_printer(days_in_month, first_day):
         # New line after every 7th day (including spaces)
         if (i + first_day - 1) % day_in_week == 0:
             print()  # newline
-
     return calender_title
 
 if __name__ == "__main__":
